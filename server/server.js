8f9ffe3ee4873e837cdedae9c0ddfc492c85eaba
Daniel Tibbotts <52741304+minlopalis@users.noreply.github.com>
2022-03-08 08:50:12 +1300

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,33 @@
 require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+const http = require("http");
+
 const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+app.use(cors());
+
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Socket.io");
